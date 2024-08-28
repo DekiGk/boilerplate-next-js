df7f1f59594d917f777127bd0c6e846d87e8919f
DekiGk <dekigk@gmail.com>
2024-08-28 22:52:30 +0200

    improve test coverage

---
+++ __tests__/first-post.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import FirstPost from "../pages/posts/first-post";
+
+describe("First Post", () => {
+  it("should render all elements correctly", () => {
+    const { getByText } = render(<FirstPost />);
+
+    expect(getByText("First Post")).toBeInTheDocument();
+    expect(getByText("← Back to home")).toBeInTheDocument();
+  });
+});
